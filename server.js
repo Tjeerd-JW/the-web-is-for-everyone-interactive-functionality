1e61e17ba2aa80ce65156073dc19ccdff3adec62
Tjeerd Wokke <tj.wokke@gmail.com>
2026-03-16 09:20:12 +0100

    initial commit

--- server.js
+++ server.js
@@ -5,77 +5,175 @@ import express from 'express'
 // Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
 import { Liquid } from 'liquidjs';
 
+
+// console.log('Hieronder moet je waarschijnlijk nog wat veranderen')
+// Doe een fetch naar de data die je nodig hebt
+// const apiResponse = await fetch('...')
+
+// Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
+// const apiResponseJSON = await apiResponse.json()
+
+// Controleer eventueel de data in je console
+// (Let op: dit is _niet_ de console van je browser, maar van NodeJS, in je terminal)
+// console.log(apiResponseJSON)
+
+
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express()
 
+const apiURL = 'https://fdnd-agency.directus.app/items/adconnect_'
+
+
 // Maak werken met data uit formulieren iets prettiger
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 // Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
 // Bestanden in deze map kunnen dus door de browser gebruikt worden
 app.use(express.static('public'))
 
 // Stel Liquid in als 'view engine'
-const engine = new Liquid()
-app.engine('liquid', engine.express())
+const engine = new Liquid();
+app.engine('liquid', engine.express());
 
 // Stel de map met Liquid templates in
 // Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
 app.set('views', './views')
 
-
-console.log('Let op: Er zijn nog geen routes. Voeg hier dus eerst jouw GET en POST routes toe.')
-
-/*
-// Zie https://expressjs.com/en/5x/api.html#app.get.method over app.get()
-app.get(…, async function (request, response) {
-  
-  // Zie https://expressjs.com/en/5x/api.html#res.render over response.render()
-  response.render(…)
+// Maak een GET route voor de index (meestal doe je dit in de root, als /)
+app.get('/', async function (request, response) {
+   // Render index.liquid uit de Views map
+   // Geef hier eventueel data aan mee
+   response.render('index.liquid')
 })
-*/
 
-/*
-// Zie https://expressjs.com/en/5x/api.html#app.post.method over app.post()
-app.post(…, async function (request, response) {
-
-  // In request.body zitten alle formuliervelden die een `name` attribuut hebben in je HTML
-  console.log(request.body)
-
-  // Via een fetch() naar Directus vullen we nieuwe gegevens in
-
-  // Zie https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch over fetch()
-  // Zie https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify over JSON.stringify()
-  // Zie https://docs.directus.io/reference/items.html#create-an-item over het toevoegen van gegevens in Directus
-  // Zie https://docs.directus.io/reference/items.html#update-an-item over het veranderen van gegevens in Directus
-  const fetchResponse = await fetch(…, {
-    method: …,
-    body: JSON.stringify(…),
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8'
-    }
-  })
-
-  // Als de POST niet gelukt is, kun je de response loggen. Sowieso een goede debugging strategie.
-  // console.log(fetchResponse)
-
-  // Eventueel kun je de JSON van die response nog debuggen
-  // const fetchResponseJSON = await fetchResponse.json()
-  // console.log(fetchResponseJSON)
-
-  // Redirect de gebruiker daarna naar een logische volgende stap
-  // Zie https://expressjs.com/en/5x/api.html#res.redirect over response.redirect()
-  response.redirect(303, …)
+// Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
+// Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
+app.post('/', async function (request, response) {
+   // Je zou hier data kunnen opslaan, of veranderen, of wat je maar wilt
+   // Er is nog geen afhandeling van een POST, dus stuur de bezoeker terug naar /
+   response.redirect(303, '/')
 })
-*/
-
 
 // Stel het poortnummer in waar Express op moet gaan luisteren
-// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
+// Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
 app.set('port', process.env.PORT || 8000)
 
-// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+// Start Express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
-  // Toon een bericht in de console
-  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
+   // Toon een bericht in de console en geef het poortnummer door
+   console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+
+app.get('/talent-awards', async function (request, response) {
+   const awardsResponse = await fetch(apiURL + 'nominations')
+   const awardsResponseJSON = await awardsResponse.json()
+   response.render('talent-awards.liquid', {
+      nominations: awardsResponseJSON.data,
+      path: request.path
+   })
+})
+
+app.get('/talent-awards/:year', async function (request, response) {
+   const awardsResponse = await fetch(apiURL + 'nominations')
+   const awardsResponseJSON = await awardsResponse.json()
+   response.render('talent-awards-year.liquid', {
+      nominations: awardsResponseJSON.data,
+      path: request.path
+   })
+})
+
+
+
+app.get('/talent-awards/:year/:name', async function (request, response) {
+
+   // uncomment wanneer api het doet
+   // const candidateParams = new URLSearchParams( {
+   //    'filter[title]': request.params.name
+   // })
+   // const candidateResponse = await fetch(apiURL + 'nominations?' + candidateParams)
+   // const candidateResponseJSON = await candidateResponse.json()
+
+   // fake api data
+   const fakeData = [
+      {
+         id: 1,
+         body: '<p>Mijn naam is Rhod&eacute; Treur, 22 jaar oud, woon in Woudenberg en ik volg de Ad-opleiding Human Resource Management aan de Hogeschool Utrecht. Momenteel zit ik in het tweede jaar van deze Ad-opleiding en ben ik bezig met mijn eindstage bij Timon jeugd- en (jong)volwassenenzorg. In mijn vrije tijd werk ik als douanedeclarant bij een transportbedrijf en sport ik graag. &nbsp;<br>&nbsp;<br>Ik heb voor een Ad-opleiding gekozen, omdat je hier de kennis en vaardigheden die je opdoet, direct in de praktijk kan brengen door middel van projecten. Ook is er in de opleiding veel aandacht voor je persoonlijke ontwikkeling. Daarnaast vind ik het een voordeel dat het een tweejarige hbo-opleiding is, met de mogelijkheid om door te stromen in de bachelorvariant. &nbsp;<br>&nbsp;<br>Mijn keuze voor Human Resource Management komt voort uit de wens om het menselijke en het zakelijke aspect te combineren. Ik zie het als een uitdaging om medewerkers op de beste manier in te zetten binnen een organisatie, zodat zij op een plek zitten waar ze gelukkig zijn en waar hun talenten het beste uitkomen. Binnen de opleiding vraag ik actief om feedback van zowel medestudenten als docenten. Aan de hand daarvan reflecteer ik op mijn denken en handelen en stel ik doelen op die bijdragen aan mijn professionele ontwikkeling. &nbsp;<br>&nbsp;<br>Na het afronden van mijn Ad-opleiding zie ik mijzelf doorstromen in de deeltijdvariant van de bachelor. Hierdoor kan ik mijn kennis en vaardigheden dagelijks toepassen in de praktijk en kan ik mij blijven ontwikkelen in de rol als HR-officer.&nbsp;</p>',
+         education_variant: 'Voltijd',
+         previous_course: " mbo bol",
+         course: "Ad Human Resource Management (HRM)",
+         institution: "Hogeschool Utrecht ",
+         institution_body: 'Thijs is een student op wie wij enorm trots zijn. Als SLBer heb ik Thijs leren kennen als een betrokken, bescheiden en zeer leergierige Ad-professional, die zijn vakmanschap vooral laat zien door zijn houding en handelen, en niet door zichzelf op de voorgrond te plaatsen. Voor een project in leerjaar 1 nam hij hierbij het voortouw om op zijn stage een preventieve activiteit met Alpaca’s te regelen voor cliënten, wat een groot succes was! In samenwerking met medestudenten, docenten en het werkveld is Thijs respectvol, betrouwbaar en proactief.Hij neemt verantwoordelijkheid, werkt zelfstandig en denkt actief mee over oplossingen voor praktijkvraagstukken. Zijn integriteit is hierin opvallend: Thijs oordeelt niet en blijft altijd zorgvuldig in hoe hij over anderen spreekt. Deze houding is van grote waarde in het werken met de complexe doelgroep, die vaak te maken krijgt met vooroordelen en stigma.Thijs benadert cliënten consequent oordeelloos en mensgericht. Wat Thijs daarnaast onderscheidt, is zijn sterk lerend vermogen.Hij reflecteert eerlijk op zijn eigen handelen en neemt regie over zijn ontwikkeling en vervolgstappen als Ad- professional. Dat maakt hem in onze ogen een zeer terechte kandidaat voor de Ad Talent Award 2026, want als er al een prachtige prijs als deze gegeven mag worden aan een student binnen het werkveld van Zorg en Veiligheid, dan verdiend Thijs Kiens het wel. Fransien Hummel, SLBer bij opleiding Ad Zorg en Veiligheid, óók namens het gehele docententeam en de Academie) ',
+         external_stake_holder: 'Lisa van Zuilen, sociaal beheerder bij Cazas Wonen',
+         external_stake_holder_body: '“Thijs heeft alles op orde, beantwoordt vragen met een lach en is hulpvaardig. Vanaf de start van zijn stage bij sociaal beheer neemt hij zelf de verantwoordelijkheid voor zijn leerproces. Op dag 1 kreeg hij zijn spullen, de dag erna liep hij met iemand mee en op dag 3 heeft hij, op eigen initiatief, zelfstandig bewoners bezocht. Thijs legt makkelijk contact, hij is een verbinder. Aan het begin van de stage had hij een overleg met wijkagenten, stelde zich voor als ‘de nieuwe directeur’ maar uiteraard met een lach. Het ijs was direct gebroken. Een bewoner die 40 jaar bij Cazas woonde krijgt normaliter bezoek vanuit coöperatie, dat lukte echter niet overdag en dus maakte Thijs in de avond tijd vrij om langs te gaan. Of die keer dat hij kacheltjes verzamelde toen er een gaslek op de Eikenlaan was. Kortom; Thijs is van alle markten thuis.”',
+         header: "nominatie",
+         slug: 'rhode-treur',
+         title: "Rhodé Treur",
+         date: "2024-01-11",
+         profile_picture: "383622d7-ed2f-4e84-8c0a-9476c5c53826",
+         year: 2026
+      }
+   ]
+   const candidate = fakeData.find(c => c.slug === request.params.name)
+
+   response.render('talent-candidate.liquid', {
+      // uncomment wanneer api het doet
+      // candidate: candidateResponseJSON.data[0],
+      candidate: candidate,
+      year: request.params.year
+   })
+})
+
+
+app.use((request, response, next) => {
+   //   response.status(404).send("Sorry can't find that!")
+   response.render('404.liquid')
+
+})
+
+
+// alle pagina's
+
+app.get('/over-ons', async function (request, response) {
+   response.render('over-ons.liquid')
+})
+
+app.get('/contact', async function (request, response) {
+   response.render('contact.liquid')
+})
+
+app.get('/over-ad', async function (request, response) {
+   response.render('over-ad.liquid')
+})
+
+app.get('/over-ad/faq', async function (request, response) {
+   response.render('faq.liquid')
+})
+
+app.get('/nieuws', async function (request, response) {
+   response.render('news.liquid')
+})
+
+app.get('/nieuws/:title', async function (request, response) {
+   response.render('news-article.liquid')
+})
+
+app.get('/publicaties', async function (request, response) {
+   response.render('publicaties.liquid')
+})
+
+app.get('/publicatie/:title', async function (request, response) {
+   response.render('publicatie.liquid')
+})
+
+app.get('/lado', async function (request, response) {
+   response.render('lado.liquid')
+})
+
+app.get('/events', async function (request, response) {
+   response.render('events.liquid')
+})
+
+app.get('/events/:name', async function (request, response) {
+   response.render('events.liquid')
 })
